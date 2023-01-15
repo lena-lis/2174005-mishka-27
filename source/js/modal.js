9971f1338683ed8d346d2c1f0c735e4acb7a44e8
Elena <fjernt.land@gmail.com>
2023-01-15 12:57:38 +0300

    Изменяет скрипт открытия модального окна

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,8 +1,11 @@
 let orderButton = document.querySelector(".bestseller__button");
 let modalContainer = document.querySelector(".modal-container");
 
+modalContainer.classList.add("modal-container--closed");
+
 orderButton.addEventListener("click", function () {
   if (modalContainer.classList.contains(".modal-container--closed")) {
     modalContainer.classList.remove(".modal-container--closed");
+    modalContainer.classList.add("modal-container--opened");
   }
 });
